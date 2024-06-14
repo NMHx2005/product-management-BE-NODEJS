a69032b149a7f9b843af11876835d403ef52d27c
Your Name <you@example.com>
2024-06-15 01:02:30 +0700

    Tạo model

---
+++ model/product.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+
+// Hàm để định nghĩa ra bộ khung của database. Dùng Schema để làm cho truyền được vào giá trị của một object
+const productSchema = new Schema({ 
+    title: String,
+    description: String,
+    price: Number,
+    discountPercentage: Number,
+    stock: Number,
+    thumbnail: String,
+    status: String,
+    position: Number,
+    deleted: Boolean
+});
+
+const Product = mongoose.model('Product', productSchema, "products");
+
+module.exports = Product;
