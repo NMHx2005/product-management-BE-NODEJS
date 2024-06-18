07a7a47c30389df796a255a332d26a5c9a16c4b9
NMHx2005 <22092005nguyenhung@gmail.com>
2024-06-18 08:43:01 +0700

    Tính năng phân trang - Frontend

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -60,3 +60,23 @@ if(formSearch) {
   });
 }
 // End From search
+
+
+
+
+// Pagination
+const listButtonPagination = document.querySelectorAll("[button-pagination]");
+if(listButtonPagination.length > 0) {
+  let url = new URL(window.location.href);
+
+  listButtonPagination.forEach(button => {
+    button.addEventListener("click", () => {
+      const page = button.getAttribute("button-pagination");
+
+      url.searchParams.set("page", page);
+
+      window.location.href = url.href;
+    });
+  });
+}
+// End Pagination
