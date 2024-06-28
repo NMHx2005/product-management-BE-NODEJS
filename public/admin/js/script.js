1ce90d09fa6cbdd2bfe60c0d130b3dcb38aaa3eb
NMHx2005 <22092005nguyenhung@gmail.com>
2024-06-28 14:10:42 +0700

    Thay đổi trạng thái nhiều sản phẩm - Tối ưu code

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -169,7 +169,9 @@ if (boxActions) {
         status: status,
       };
 
-      fetch("/admin/products/change-multi", {
+      const link = boxActions.getAttribute("box-actions");
+
+      fetch(link, {
         method: "PATCH",
         headers: {
           "Content-Type": "application/json",
