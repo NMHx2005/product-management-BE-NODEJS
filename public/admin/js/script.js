690b2f1c1315c1e1ed879411a3dce69b5113f664
NMHx2005 <22092005nguyenhung@gmail.com>
2024-06-28 15:32:08 +0700

    Tối ưu code - Tối ưu phần xóa

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -198,9 +198,9 @@ const listButtonDelete = document.querySelectorAll("[button-delete]");
 if (listButtonDelete.length > 0) {
   listButtonDelete.forEach(button => {
     button.addEventListener("click", () => {
-      const id = button.getAttribute("button-delete");
+      const link = button.getAttribute("[button-delete]")
 
-      fetch(`/admin/products/delete/${id}`, {
+      fetch(link , {
         method: "PATCH"
       })
         .then(res => res.json())
