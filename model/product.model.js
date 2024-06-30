c78a303a9b4b2ae42089a6fc7eadb8a3c96cc35f
NMHx2005 <22092005nguyenhung@gmail.com>
2024-06-30 15:26:47 +0700

    Tính năng thêm mới sản phẩm - tự động thêm createdAt và updatedAt và slug tự động sinh ra

--- model/product.model.js
+++ model/product.model.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-
+const slug = require('mongoose-slug-updater');
+mongoose.plugin(slug);
 
 // Hàm để định nghĩa ra bộ khung của database. Dùng Schema để làm cho truyền được vào giá trị của một object
 const productSchema = new Schema({ 
@@ -15,7 +16,14 @@ const productSchema = new Schema({
     deleted: {
         type: Boolean,
         default: false
+    }, 
+    slug: {
+        type: String,
+        slug: "title",
+        unique: true
     }
+}, {
+    timestamps: true // Tự động thêm trường createdAt và updatedAt.
 });
 
 const Product = mongoose.model('Product', productSchema, "products");
