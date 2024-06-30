644f380d633c390303493b295e1939b7b782a172
NMHx2005 <22092005nguyenhung@gmail.com>
2024-06-30 20:21:40 +0700

    Tính năng tạo mới sản phẩm - Preview ảnh

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -265,3 +265,20 @@ if(showAlert) {
 // End show-alert
 
 
+// Upload image
+const uploadImage = document.querySelector("[upload-image]");
+if (uploadImage) {
+  const uploadImageInput = uploadImage.querySelector("[upload-image-input]");
+  const uploadImagePreview = uploadImage.querySelector("[upload-image-preview]");
+
+  uploadImageInput.addEventListener("change", () => {
+    const file = uploadImageInput.files[0];
+    
+    if (file) {
+      uploadImagePreview.src = URL.createObjectURL(file);
+    }
+  });
+
+
+}
+// End Upload image
