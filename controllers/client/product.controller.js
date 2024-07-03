189fa8f2bdb55a23fe29982cd58b4ae71745a40c
NMHx2005 <22092005nguyenhung@gmail.com>
2024-07-03 12:53:29 +0700

    Làm trang chi tiết sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -23,3 +23,26 @@ module.exports.index = async (req, res) => {
         products: products
     });
 }
+
+
+// [GET] /products/detail
+module.exports.detail = async (req, res) => {
+    const slug = req.params.slug;
+
+
+    const product = await Product.findOne({
+        slug: slug,
+        deleted: false,
+        status: "active"
+    });
+
+
+    if(product) {
+        res.render("client/pages/products/detail", {
+          pageTitle: "Chi tiết sản phẩm",
+          product: product
+        });
+    } else {
+        res.redirect("/");
+    }
+}
