b0f14a887c32f76de4485c659f77de56e5c5c11e
NMHx2005 <22092005nguyenhung@gmail.com>
2024-07-19 15:28:04 +0700

    Làm trang sửa tài khoản

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -58,4 +58,46 @@ module.exports.createPost = async (req, res) => {
 
     // Sau khi hoàn thành thì quay lại trang nì
     res.redirect(`/${systemConfig.prefixAdmin}/accounts`);
-  }
+}
+
+
+// [GET] /admin/accounts/edit/:id
+module.exports.edit = async (req, res) => {
+    const id = req.params.id;
+
+    const account = await Account.findOne({
+        _id: id,
+        deleted: false
+    });
+  
+    const roles = await Role.find({
+        deleted: false
+    }).select("title");
+
+    res.render("admin/pages/accounts/edit", {
+      pageTitle: "Chỉnh sửa tài khoản admin",
+      roles: roles,
+      account: account
+    });
+}
+
+
+// [PATCH] /admin/accounts/edit/:id
+module.exports.editPatch = async (req, res) => {
+    const id = req.params.id;
+    
+    if(req.body.password == "") {
+        delete req.body.password;
+    } else {
+        req.body.password = md5(req.body.password);
+    }
+
+    await Account.updateOne({
+        _id: id,
+        deleted: false
+    }, req.body);
+
+    req.flash("success", "Chỉnh sửa tài khoản thành công!");
+
+    res.redirect(`/${systemConfig.prefixAdmin}/accounts`);
+}
