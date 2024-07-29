1bc193a83223eca6b256289b6c22c8bd1815c8d9
NMHx2005 <22092005nguyenhung@gmail.com>
2024-07-29 23:11:04 +0700

    Tạo model

---
+++ model/articles.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const slug = require('mongoose-slug-updater');
+mongoose.plugin(slug);
+
+// Hàm để định nghĩa ra bộ khung của database. Dùng Schema để làm cho truyền được vào giá trị của một object
+const articleSchema = new Schema({ 
+    title: String,
+    description: String,
+    thumbnail: String,
+    status: String,
+    position: Number,
+    deleted: {
+        type: Boolean,
+        default: false
+    }, 
+    slug: {
+        type: String,
+        slug: "title",
+        unique: true
+    }
+}, {
+    timestamps: true // Tự động thêm trường createdAt và updatedAt.
+});
+
+const Article = mongoose.model('Article', articleSchema, "articles");
+
+module.exports = Article;
