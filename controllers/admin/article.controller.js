1ac4b1f06bf016177dfddb27ca758cd5a2a8b704
NMHx2005 <22092005nguyenhung@gmail.com>
2024-07-30 08:58:26 +0700

    Lọc trạng thái

--- controllers/admin/article.controller.js
+++ controllers/admin/article.controller.js
@@ -5,14 +5,35 @@ const Article = require("../../model/articles.model")
 
 // [GET] /admin/pages/articles
 module.exports.index = async (req, res) => {
-    const articles = await Article.find({
+    const find = {
         deleted: false
-    })
+    }
 
-    // console.log(articles);
+    if(req.query.status) {
+        find.status = req.query.status;
+    }
+
+    const articles = await Article.find(find);
+
+    // Tối ưu hóa phần Bộ lọc
+    const filterStatus = [
+        {
+        label: "Tất cả",
+        value: ""
+        },
+        {
+        label: "Hoạt động",
+        value: "active"
+        },
+        {
+        label: "Dừng hoạt động",
+        value: "inactive"
+        }
+    ];
+
     res.render("admin/pages/articles/index", {
         pageTitle: "Quản lý bài viết",
-        articles: articles
+        articles: articles,
+        filterStatus: filterStatus
     });
-    // res.send("OK");
 }
