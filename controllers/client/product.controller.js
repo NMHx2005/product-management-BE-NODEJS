07ac966fa71bd7b8b5884081f50239652bc413a2
NMHx2005 <22092005nguyenhung@gmail.com>
2024-08-02 00:36:28 +0700

    Trang danh sách sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,4 +1,6 @@
 const Product = require("../../model/product.model");
+const ProductCategory = require("../../model/product-category.model");
+
 
 // [GET] /products/
 module.exports.index = async (req, res) => {
@@ -45,4 +47,58 @@ module.exports.detail = async (req, res) => {
     } else {
         res.redirect("/");
     }
+}
+
+
+
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+
+    const slugCategory = req.params.slugCategory;
+
+    const category = await ProductCategory.findOne({
+        slug: slugCategory,
+        deleted: false,
+        status: "active"
+    });
+
+    const allSubCategory = [];
+
+    const getSubCategory = async (currentId) => {
+        const subCategory = await ProductCategory.find({
+            parent_id: currentId,
+            deleted: false,
+            status: "active"
+        })
+
+        for (const item of subCategory) {
+            allSubCategory.push(item.id);
+            await getSubCategory(item.id);
+        }
+    }
+
+    await getSubCategory(category.id);
+
+    const products = await Product
+        .find({
+            product_category_id: {
+                $in: [
+                    category.id,
+                    ...allSubCategory
+                ]
+            }
+        })
+        .sort({
+            position: "desc"
+        });
+    
+    for (const item of products) {
+        item.priceNew = ((1 - item.discountPercentage/100) * item.price).toFixed(0);
+    }
+
+    res.render("client/pages/products/index", {
+        pageTitle: category.title,
+        products: products
+    })
+
 }
