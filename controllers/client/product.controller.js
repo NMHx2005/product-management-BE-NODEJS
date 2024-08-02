618004da9706228464afb20f753cb0ebf722f8aa
NMHx2005 <22092005nguyenhung@gmail.com>
2024-08-02 12:33:57 +0700

    Trang chi tiết sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -38,6 +38,7 @@ module.exports.detail = async (req, res) => {
         status: "active"
     });
 
+    product.priceNew = ((1 - product.discountPercentage/100) * product.price).toFixed(0);
 
     if(product) {
         res.render("client/pages/products/detail", {
