59447cc1c05bd476a740f31694b90494ae54cf47
NMHx2005 <22092005nguyenhung@gmail.com>
2024-08-03 22:18:38 +0700

    Làm trang giỏ hàng +  Xóa sản phẩm trong giỏ hàng +  Cập nhật số lượng sản phẩm trong giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -10,3 +10,20 @@ if (arletAddCart) {
     });
 }
 // Kết thúc thông báo sau khi thêm sản phẩm vào giỏ hàng
+
+
+// Cập nhật số lượng sản phẩm trong giỏ hàng
+const listInputQuantity = document.querySelectorAll("[cart] input[name='quantity']");
+if(listInputQuantity.length > 0) {
+  listInputQuantity.forEach(input => {
+    input.addEventListener("change", () => {
+      const productId = input.getAttribute("product-id");
+      const quantity = parseInt(input.value);
+
+      if(productId && quantity > 0) {
+        window.location.href = `/cart/update/${productId}/${quantity}`;
+      }
+    })
+  })
+}
+// Hết Cập nhật số lượng sản phẩm trong giỏ hàng
