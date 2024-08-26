e2fcb31d9892b4be65b7c4a7667010f4af52e9c2
NMHx2005 <22092005nguyenhung@gmail.com>
2024-08-26 16:03:39 +0700

    Hiển thị ảnh full màn hình

--- public/js/chat.js
+++ public/js/chat.js
@@ -118,6 +118,8 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
   body.insertBefore(div, elementListTyping);
 
   body.scrollTop = body.scrollHeight;
+
+  new Viewer(div);
 });
 // End SERVER_RETURN_MESSAGE
 
@@ -151,3 +153,9 @@ if(buttonIcon) {
   });
 }
 // End Show Popup Icon
+
+// Preview Image
+if(bodyChat) {
+  new Viewer(bodyChat);
+}
+// End Preview Image
